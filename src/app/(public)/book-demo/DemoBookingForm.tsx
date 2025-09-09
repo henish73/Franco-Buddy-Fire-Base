56031e17e972c9e57a3bf22fe9bbd868ddd8d45e
Henish Patel <phenish890@gmail.com>
2025-09-09 01:17:45 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: (0 , {imported m

--- src/app/(public)/book-demo/DemoBookingForm.tsx
+++ src/app/(public)/book-demo/DemoBookingForm.tsx
@@ -2,7 +2,8 @@
 "use client";
 
 import { useState } from 'react';
-import { useActionState, useFormStatus } from "react";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
